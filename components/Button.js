2de37fb2a9a8f53503c131502b3d88e89404d9f6
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-08-07 22:48:08 -0300

    aula 07-08

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,14 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
     if (theme === "primary") {
         return (
             <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
+                 
                 <Pressable 
                     style={[styles.button, {backgroundColor: "#fff"}]} 
-                    onPress={() => alert('You pressed a button.')}
+                    onPress={onPress}
                 >
                     <FontAwesome
                         name="picture-o"
@@ -20,6 +21,7 @@ export default function Button({ label, theme }) {
             </View>
         );
     }
+
 
     return (
         <View style={styles.buttonContainer}>
